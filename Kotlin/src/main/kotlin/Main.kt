bcd878e217c058d4f79378e554df67ce0da313c3
Jesper Evertsson <jesper@piktiv.se>
2024-12-07 11:05:00 +0100

    Add solution for day 6

--- Kotlin/src/main/kotlin/Main.kt
+++ Kotlin/src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 import java.io.File
 import kotlin.system.measureTimeMillis
 
-val daySolvers = listOf(::day6, ::day7, ::day8, ::day9, ::day10, ::day11, ::day12, ::day13, ::day14, ::day15, ::day16, ::day17, ::day18, ::day19, ::day20, ::day21, ::day22, ::day23, ::day24, ::day25)
+val daySolvers = listOf(::day1, ::day2, ::day3, ::day4, ::day5, ::day6, ::day7, ::day8, ::day9, ::day10, ::day11, ::day12, ::day13, ::day14, ::day15, ::day16, ::day17, ::day18, ::day19, ::day20, ::day21, ::day22, ::day23, ::day24, ::day25)
 
-val skipDays = listOf(0)
+val skipDays = listOf(6)
 
 fun main(args: Array<String>) {
     val timings = mutableMapOf<String, Long>()
--- Kotlin/src/main/kotlin/day6.kt
+++ Kotlin/src/main/kotlin/day6.kt
@@ -1,3 +1,144 @@
 fun day6 (lines: List<String>) {
-
+    println()
+    day6part1(lines)
+    day6part2(lines)
 }
+
+fun day6part1(lines: List<String>) {
+    val obstaclePositions = mutableListOf<Position>()
+    var guard = Guard(Position(0,0), Direction.UP)
+
+    lines.forEachIndexed { y, line ->
+        line.forEachIndexed { x, cell ->
+            if (cell == '#') {
+                obstaclePositions.add(Position(x, y))
+            } else if (cell == '^') {
+                guard.position = Position(x, y)
+            }
+        }
+    }
+
+    val size = lines.size
+    val visitedPositions = mutableSetOf(guard.position)
+    val previousGuardStates = mutableSetOf(Guard(guard.position, guard.direction))
+
+    while (inBounds(size, guard)) {
+        while (turnIfNeeded(guard, obstaclePositions)) {
+            previousGuardStates.add(Guard(guard.position, guard.direction))
+        }
+        move(guard)
+        visitedPositions.add(guard.position)
+        previousGuardStates.add(Guard(guard.position, guard.direction))
+    }
+
+    println("Day 6 part 1: ${visitedPositions.size - 1}")
+}
+
+fun day6part2(lines: List<String>) {
+    val obstaclePositions = mutableListOf<Position>()
+    var guard = Guard(Position(0,0), Direction.UP)
+
+    lines.forEachIndexed { y, line ->
+        line.forEachIndexed { x, cell ->
+            if (cell == '#') {
+                obstaclePositions.add(Position(x, y))
+            } else if (cell == '^') {
+                guard.position = Position(x, y)
+            }
+        }
+    }
+
+    val size = lines.size
+    var options = 0
+
+    lines.forEachIndexed { y, line ->
+        line.forEachIndexed { x, cell ->
+            if (cell == '.') {
+                val enhancedObstaclePositions = obstaclePositions.toMutableList()
+                enhancedObstaclePositions.add(Position(x, y))
+                val guardCopy = Guard(Position(guard.position.x, guard.position.y), guard.direction)
+                var steps = 0
+
+                while (inBounds(size, guardCopy)) {
+                    while (turnIfNeeded(guardCopy, enhancedObstaclePositions)) {
+                    }
+                    move(guardCopy)
+                    steps++
+                    if (steps > 7000) {
+                        options++
+                        break
+                    }
+                }
+            }
+        }
+    }
+
+    println("Day 6 part 2: $options")
+}
+
+fun inBounds(size: Int, guard: Guard): Boolean {
+    if (guard.position.x < 0) {
+        return false
+    }
+
+    if (guard.position.y < 0) {
+        return false
+    }
+
+    if (guard.position.x == size) {
+        return false
+    }
+
+    if (guard.position.y == size) {
+        return false
+    }
+
+    return true
+}
+
+fun move(guard: Guard) {
+    when (guard.direction) {
+        Direction.DOWN -> guard.position = Position(guard.position.x, guard.position.y + 1)
+        Direction.UP -> guard.position = Position(guard.position.x, guard.position.y - 1)
+        Direction.LEFT -> guard.position = Position(guard.position.x - 1, guard.position.y)
+        Direction.RIGHT -> guard.position = Position(guard.position.x + 1, guard.position.y)
+    }
+}
+
+fun turnIfNeeded(guard: Guard, obstaclePositions: MutableList<Position>): Boolean {
+    when (guard.direction) {
+        Direction.DOWN -> {
+            if (obstaclePositions.contains(Position(guard.position.x, guard.position.y + 1))) {
+                guard.direction = Direction.LEFT
+                return true
+            }
+        }
+        Direction.UP -> {
+            if (obstaclePositions.contains(Position(guard.position.x, guard.position.y - 1))) {
+                guard.direction = Direction.RIGHT
+                return true
+            }
+        }
+        Direction.LEFT -> {
+            if (obstaclePositions.contains(Position(guard.position.x - 1, guard.position.y))) {
+                guard.direction = Direction.UP
+                return true
+            }
+        }
+        Direction.RIGHT -> {
+            if (obstaclePositions.contains(Position(guard.position.x + 1, guard.position.y))) {
+                guard.direction = Direction.DOWN
+                return true
+            }
+        }
+    }
+    return false
+}
+
+data class Position (val x: Int, val y: Int)
+
+enum class Direction {
+    UP, DOWN, LEFT, RIGHT
+}
+
+data class Guard (var position: Position, var direction: Direction)
