9ccba48070e4b347cfca45873258f5d196a8a997
Jesper Evertsson <jesper@piktiv.se>
2024-12-10 08:00:11 +0100

    Add solution for day 10 part 2

--- Kotlin/src/main/kotlin/day10.kt
+++ Kotlin/src/main/kotlin/day10.kt
@@ -24,39 +24,38 @@ fun day10 (lines: List<String>) {
 }
 
 fun day10part1(map: List<List<Int>>, trailStarts: List<TrailCoordinate>) {
-    var answer = 0
+    var part1answer = 0
+    var part2answer = 0
     
     trailStarts.forEach{
         val peaks = mutableSetOf<TrailCoordinate>()
-        findTrailCount(map, it, 0, peaks)
-        answer += peaks.size
+        part2answer += findTrailCount(map, it, 0, peaks)
+        part1answer += peaks.size
     }
     
-    println("Day 10 part 1: $answer")
+    println("Day 10 part 1: $part1answer")
+    println("Day 10 part 2: $part2answer")
 }
 
 fun day10part2() {
 
 }
 
-fun findTrailCount(map: List<List<Int>>, position: TrailCoordinate, targetHeight: Int, peaks: MutableSet<TrailCoordinate>) {
+fun findTrailCount(map: List<List<Int>>, position: TrailCoordinate, targetHeight: Int, peaks: MutableSet<TrailCoordinate>): Int {
     if (position.x < 0 || position.y < 0 || position.x >= map.size || position.y >= map.size) { 
-        return 
+        return 0 
     }
     
     if (map[position.y][position.x] != targetHeight) { 
-        return 
+        return 0 
     }
     
     if (map[position.y][position.x] == 9) {
         peaks.add(TrailCoordinate(position.x, position.y))
-        return 
+        return 1 
     }
     
-    findTrailCount(map, TrailCoordinate(position.x + 1, position.y), targetHeight + 1, peaks)
-    findTrailCount(map, TrailCoordinate(position.x - 1, position.y), targetHeight + 1, peaks)
-    findTrailCount(map, TrailCoordinate(position.x, position.y + 1), targetHeight + 1, peaks)
-    findTrailCount(map, TrailCoordinate(position.x, position.y - 1), targetHeight + 1, peaks)
+    return findTrailCount(map, TrailCoordinate(position.x + 1, position.y), targetHeight + 1, peaks) + findTrailCount(map, TrailCoordinate(position.x - 1, position.y), targetHeight + 1, peaks) + findTrailCount(map, TrailCoordinate(position.x, position.y + 1), targetHeight + 1, peaks) + findTrailCount(map, TrailCoordinate(position.x, position.y - 1), targetHeight + 1, peaks)
 }
 
 data class TrailCoordinate(val x: Int, val y: Int)
