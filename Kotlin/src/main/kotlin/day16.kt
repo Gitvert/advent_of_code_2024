9a06fa3f182e6209102a5ffac4e08f0a486e66fa
Jesper Evertsson <jesper@piktiv.se>
2024-12-20 21:57:06 +0100

    Add solution for day 16 part 2

--- Kotlin/src/main/kotlin/day16.kt
+++ Kotlin/src/main/kotlin/day16.kt
@@ -2,30 +2,27 @@ import java.util.PriorityQueue
 
 fun day16 (lines: List<String>) {
     val start = findStartCoordinate(lines)!!
-    val end = findEndCoordinate(lines)!!
     
     val mazeMap = parseMazeMap(lines)
-    val reindeer = Reindeer(start, 0, Direction.RIGHT)
+    val reindeer = Reindeer(start, 0, Direction.RIGHT, mutableListOf())
     
-    val part1Answer = findShortestPath(mazeMap, end, reindeer)
+    val answer = findShortestPath(mazeMap, reindeer)
     
-    println("Day 16 part 1: $part1Answer")
+    println("Day 16 part 1: ${answer.first}")
+    println("Day 16 part 2: ${answer.second.size}")
 }
 
-fun findAllShortestPaths(mazeMap: List<List<Char>>, end: Position, reindeer: Reindeer) {
-    
-}
-
-fun findShortestPath(mazeMap: List<List<Char>>, end: Position, reindeer: Reindeer): Int {
+fun findShortestPath(mazeMap: List<List<Char>>, reindeer: Reindeer): Pair<Int, Set<Position>> {
     val dx = arrayOf(-1, 0, 1, 0)
     val dy = arrayOf(0, 1, 0, -1)
-    val ySize = mazeMap.size
-    val xSize = mazeMap[0].size
-    val distance = Array(ySize) { IntArray(xSize) { Int.MAX_VALUE } }
 
-    distance[reindeer.pos.y][reindeer.pos.x] = 0
+    val distance = mutableMapOf<Pair<Position, Direction>, Int>().withDefault { Int.MAX_VALUE }
+    var lowest = Int.MAX_VALUE
+    val paths = mutableSetOf(reindeer.pos)
+    
+    distance[Pair(reindeer.pos, Direction.RIGHT)] = 0
 
-    val pq: PriorityQueue<Reindeer> = PriorityQueue(xSize * ySize, compareBy { it.cost })
+    val pq = PriorityQueue<Reindeer>(compareBy { it.cost })
 
     pq.add(reindeer)
 
@@ -35,25 +32,27 @@ fun findShortestPath(mazeMap: List<List<Char>>, end: Position, reindeer: Reindee
         for (i in 0..3) {
             val targetX = current.pos.x + dx[i]
             val targetY = current.pos.y + dy[i]
-
+            
             val newReindeerPosition = costToMoveTo(Position(targetX, targetY), mazeMap, current)
-            if (isInGrid(Position(targetX, targetY), xSize, ySize) && newReindeerPosition != null) {
-                if (distance[targetY][targetX] > distance[current.pos.y][current.pos.x] + newReindeerPosition.cost) {
+            if (newReindeerPosition != null) {
+                if (distance.getValue(Pair(newReindeerPosition.pos, newReindeerPosition.dir)) >= current.cost + newReindeerPosition.cost) {
 
-                    if (distance[targetX][targetX] != Int.MAX_VALUE) {
-                        val adj = Reindeer(Position(targetX, targetY), distance[targetY][targetX], current.dir)
-                        pq.remove(adj)
+                    distance[Pair(newReindeerPosition.pos, newReindeerPosition.dir)] = current.cost + newReindeerPosition.cost
+
+                    if (mazeMap[targetY][targetX] == 'E') {
+                        if (current.cost + newReindeerPosition.cost <= lowest) {
+                            lowest = current.cost + newReindeerPosition.cost
+                            paths.addAll(newReindeerPosition.path)
+                        }
+                    } else {
+                        pq.add(Reindeer(Position(targetX, targetY), current.cost + newReindeerPosition.cost, newReindeerPosition.dir, newReindeerPosition.path))
                     }
-
-                    // Insert cell with updated distance
-                    distance[targetY][targetX] = distance[current.pos.y][current.pos.x] + newReindeerPosition.cost
-                    pq.add(Reindeer(Position(targetX, targetY), distance[targetY][targetX], newReindeerPosition.dir))
                 }
             }
         }
     }
 
-    return distance[end.y][end.x]
+    return lowest to paths
 }
 
 fun costToMoveTo(target: Position, mazeMap: List<List<Char>>, current: Reindeer): Reindeer? {
@@ -61,56 +60,55 @@ fun costToMoveTo(target: Position, mazeMap: List<List<Char>>, current: Reindeer)
         return null
     }
     
+    val newPath = current.path.toMutableList()
+    newPath.add(target)
+    
     return when (current.dir) {
         Direction.RIGHT -> {
             if (target.x > current.pos.x) {
-                Reindeer(Position(target.x, target.y), 1, Direction.RIGHT)
+                Reindeer(Position(target.x, target.y), 1, Direction.RIGHT, newPath)
             } else if (target.y < current.pos.y) {
-                Reindeer(Position(target.x, target.y), 1001, Direction.UP)
+                Reindeer(Position(target.x, target.y), 1001, Direction.UP, newPath)
             } else if (target.y > current.pos.y) {
-                Reindeer(Position(target.x, target.y), 1001, Direction.DOWN)
+                Reindeer(Position(target.x, target.y), 1001, Direction.DOWN, newPath)
             } else {
                 null
             }
         }
         Direction.LEFT -> {
             if (target.x < current.pos.x) {
-                Reindeer(Position(target.x, target.y), 1, Direction.LEFT)
+                Reindeer(Position(target.x, target.y), 1, Direction.LEFT, newPath)
             } else if (target.y < current.pos.y) {
-                Reindeer(Position(target.x, target.y), 1001, Direction.UP)
+                Reindeer(Position(target.x, target.y), 1001, Direction.UP, newPath)
             } else if (target.y > current.pos.y) {
-                Reindeer(Position(target.x, target.y), 1001, Direction.DOWN)
+                Reindeer(Position(target.x, target.y), 1001, Direction.DOWN, newPath)
             } else {
                 null
             }
         }
         Direction.DOWN -> {
             if (target.y > current.pos.y) {
-                Reindeer(Position(target.x, target.y), 1, Direction.DOWN)
+                Reindeer(Position(target.x, target.y), 1, Direction.DOWN, newPath)
             } else if (target.x < current.pos.x) {
-                Reindeer(Position(target.x, target.y), 1001, Direction.LEFT)
+                Reindeer(Position(target.x, target.y), 1001, Direction.LEFT, newPath)
             } else if (target.x > current.pos.x) {
-                Reindeer(Position(target.x, target.y), 1001, Direction.RIGHT)
+                Reindeer(Position(target.x, target.y), 1001, Direction.RIGHT, newPath)
             } else {
                 null
             }
         }
         Direction.UP -> {
             if (target.y < current.pos.y) {
-                Reindeer(Position(target.x, target.y), 1, Direction.UP)
+                Reindeer(Position(target.x, target.y), 1, Direction.UP, newPath)
             } else if (target.x < current.pos.x) {
-                Reindeer(Position(target.x, target.y), 1001, Direction.LEFT)
+                Reindeer(Position(target.x, target.y), 1001, Direction.LEFT, newPath)
             } else if (target.x > current.pos.x) {
-                Reindeer(Position(target.x, target.y), 1001, Direction.RIGHT)
+                Reindeer(Position(target.x, target.y), 1001, Direction.RIGHT, newPath)
             } else {
                 null
             }
         }
     }
-}
-
-fun isInGrid(pos: Position, xSize: Int, ySize: Int): Boolean {
-    return pos.x in 0 until xSize && pos.y in 0 until ySize
 }
 
 fun findStartCoordinate(lines: List<String>): Position? {
@@ -122,18 +120,6 @@ fun findStartCoordinate(lines: List<String>): Position? {
         }
     }
         
-    return null
-}
-
-fun findEndCoordinate(lines: List<String>): Position? {
-    lines.forEachIndexed { y, row ->
-        row.forEachIndexed { x, cell ->
-            if (cell == 'E') {
-                return Position(x, y)
-            }
-        }
-    }
-
     return null
 }
 
@@ -151,4 +137,5 @@ fun parseMazeMap(lines: List<String>): MutableList<MutableList<Char>> {
     return mazeMap
 }
 
-data class Reindeer (val pos: Position, val cost: Int, val dir: Direction)
+data class Reindeer (val pos: Position, val cost: Int, val dir: Direction, val path: MutableList<Position>)
+
