d0fff9927d6969078dfb43b5db0fa45e68e60d5f
Jesper Evertsson <jesper@piktiv.se>
2024-12-21 22:21:55 +0100

    Add solution for day 19 part 1

--- Kotlin/src/main/kotlin/day19.kt
+++ Kotlin/src/main/kotlin/day19.kt
@@ -1,3 +1,44 @@
 fun day19 (lines: List<String>) {
+    val towels = mutableListOf<String>()
+    val patterns = mutableListOf<String>()
     
+    lines.forEach { 
+        if (it.contains(',')) {
+            towels.addAll(it.split(", "))
+        } else if (it.isNotEmpty()) {
+            patterns.add(it)
+        }
+    }
+
+    val possiblePatterns = patterns
+        .map { containsTowel(towels, it) }
+        .filter { it == true }
+        .count()
+    
+    println("Day 19 part 1: $possiblePatterns")
 }
+
+fun containsTowel(towels: List<String>, pattern: String): Boolean {
+    val queue = mutableSetOf<TowelPattern>()
+    
+    queue.add(TowelPattern(pattern, pattern))
+    
+    while (queue.isNotEmpty()) {
+        val towelPattern = queue.first()
+        queue.remove(towelPattern)
+        
+        towels.forEach { towel ->
+            if (towelPattern.remainingPattern.startsWith(towel)) {
+                if (towelPattern.remainingPattern.length == towel.length) {
+                    return true
+                } else {
+                    queue.add(TowelPattern(towelPattern.fullPattern, towelPattern.remainingPattern.substring(towel.length)))
+                }
+            }
+        }
+    }
+    
+    return false
+}
+
+data class TowelPattern(val fullPattern: String, val remainingPattern: String)
