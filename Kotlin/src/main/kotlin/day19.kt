7c6682e1fdf924fea3cd1ae8c74253833464ac92
Jesper Evertsson <jesper@piktiv.se>
2024-12-21 23:09:19 +0100

    Add solution for day 19 part 2

--- Kotlin/src/main/kotlin/day19.kt
+++ Kotlin/src/main/kotlin/day19.kt
@@ -1,3 +1,5 @@
+val computedPatterns = hashMapOf<String, Long>()
+
 fun day19 (lines: List<String>) {
     val towels = mutableListOf<String>()
     val patterns = mutableListOf<String>()
@@ -11,14 +13,22 @@ fun day19 (lines: List<String>) {
     }
 
     val possiblePatterns = patterns
-        .map { containsTowel(towels, it) }
+        .map { possiblePattern(towels, it) }
         .filter { it == true }
         .count()
     
     println("Day 19 part 1: $possiblePatterns")
+    
+    var combinations = 0L
+    
+    patterns.forEach { pattern ->
+        combinations += findTowelCombinations(towels, pattern)
+    }
+    
+    println("Day 19 part 2: $combinations")
 }
 
-fun containsTowel(towels: List<String>, pattern: String): Boolean {
+fun possiblePattern(towels: List<String>, pattern: String): Boolean {
     val queue = mutableSetOf<TowelPattern>()
     
     queue.add(TowelPattern(pattern, pattern))
@@ -39,6 +49,28 @@ fun containsTowel(towels: List<String>, pattern: String): Boolean {
     }
     
     return false
+}
+
+fun findTowelCombinations(towels: List<String>, pattern: String): Long {
+    if (computedPatterns.containsKey(pattern)) {
+        return computedPatterns[pattern]!!
+    }
+    
+    var combinations = 0L
+    
+    towels.forEach { towel ->
+        if (pattern.startsWith(towel)) {
+            if (pattern.length == towel.length) {
+                combinations += 1L
+            } else {
+                combinations += findTowelCombinations(towels, pattern.substring(towel.length))
+            }
+        }
+    }
+    
+    computedPatterns[pattern] = combinations
+    
+    return combinations
 }
 
 data class TowelPattern(val fullPattern: String, val remainingPattern: String)
