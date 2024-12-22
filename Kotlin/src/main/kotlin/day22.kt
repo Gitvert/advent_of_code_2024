0dff64cab781b522348146d7fb1be015806bfaef
Jesper Evertsson <jesper@piktiv.se>
2024-12-22 14:38:50 +0100

    Add solution for day 22 part 1

--- Kotlin/src/main/kotlin/day22.kt
+++ Kotlin/src/main/kotlin/day22.kt
@@ -1,3 +1,38 @@
 fun day22 (lines: List<String>) {
+    val startNumbers = lines.map { it.toLong() }
     
+    val newSecretsSum = startNumbers
+        .map { findNextSecretNumber(it, 1) }
+        .sum()
+    
+    println("Day 22 part 1: $newSecretsSum")
+}
+
+fun findNextSecretNumber(secret: Long, depth: Int): Long {
+    var newSecret = secret
+    val level1 = secret * 64
+    
+    newSecret = prune(mix(level1, newSecret))
+    
+    val level2 = newSecret / 32
+    
+    newSecret = prune(mix(level2, newSecret))
+    
+    val level3 = newSecret * 2048
+    
+    newSecret = prune(mix(level3, newSecret))
+    
+    if (depth == 2000) {
+        return newSecret
+    }
+    
+    return findNextSecretNumber(newSecret, depth + 1)
+}
+
+fun mix(value: Long, secret: Long): Long {
+    return value xor secret
+}
+
+fun prune(number: Long): Long {
+    return number % 16777216
 }
